4b6ead59facc3b56fec935fd134e389513a5b457
Sheen <shneketos@gmail.com>
2024-03-25 18:13:18 +0400

    fix

--- src/calendar_events/dto/update-calendar_event.dto.ts
+++ src/calendar_events/dto/update-calendar_event.dto.ts
@@ -1,4 +1,6 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateCalendarEventDto } from './create-calendar_event.dto';
 
-export class UpdateCalendarEventDto extends PartialType(CreateCalendarEventDto) {}
+export class UpdateCalendarEventDto extends PartialType(
+  CreateCalendarEventDto,
+) {}
